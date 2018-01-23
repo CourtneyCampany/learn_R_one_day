e540d6b366613c7c82ac5d591e739748f5e7b049
Courtney Campany <courtneycampany@gmail.com>
2018-01-23 16:30:12 -0500

    divided tutorial

---
+++ LearnR#1.R
@@ -0,0 +1,33 @@
+### Learn R assignment #1
+
+### All figures need to be uploaded as PDF's and should be of publication quality
+  ### use help files if you get stuck
+  ### Post any questions on SLACK, do not email me directly
+
+
+###(1) Using the 'data1.csv' dataframe, make a figure with raw data points and a regression line
+### make sure and code all necessary information to distguish 'Species"
+
+
+###(2) Using 'data2.csv' make a boxplot that summarizes the fuel efficiency of different cars 
+###based on the number of engine cylinders
+
+
+###(3) Using 'data3.csv' explore the relationship between height and mass of Stars Wars characters
+### that reside on planets 'Tatooine' and 'Naboo'. On your figure, please distinguish between both
+### the homeworld and species of each character
+
+
+###ONCE you have your plots finished, use:
+
+pdf("output/yourname_assignment1.pdf", onefile = TRUE)
+
+codeforplot1
+
+codeforplot2
+
+codeplot3
+
+dev.off()
+
+##submit via moodle
