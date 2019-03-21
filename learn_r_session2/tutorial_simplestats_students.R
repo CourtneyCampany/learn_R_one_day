61b6d234e7fc1c633fb2fedc33bd3c1b8005c2fd
Court Campany <ccampany@colgate.edu>
2019-03-21 10:32:02 -0400

    student scripts day 2

--- learn_r_session2/tutorial_simplestats_students.R
+++ learn_r_session2/tutorial_simplestats_students.R
@@ -1,84 +1,141 @@
-###BASIC STATS
+# BASIC STATS -----------
 
 traits <- read.csv("raw_data/fern_traits.csv")
 
 mean(traits$lamina_area_cm2)
 var_la <- var(traits$lamina_area_cm2)
-sqrt(var_la) #stand dev 0r ....
-sd(traits$lamina_area_cm2)
-median(traits$lamina_area_cm2)
-quantile(traits$lamina_area_cm2)
-#these functions might fail with NA's s0 remember na.rm=TRUE)
+sqrt(var_la)
+
+sd()
+median()
+quantile()
+#these functions might fail with NA's so remember to check for na.rm=TRUE)
 
 
-#calculate a confidence inteval (perhaps subset a specific niche)
+# calculate a confidence inteval (perhaps subset a specific niche) ---------
+
 alpha_level <- 0.05 # 95% confidence interval
-xbar <- mean(traits$lamina_area_cm2)
-s <- sd(traits$lamina_area_cm2)
-n <- length(traits$lamina_area_cm2)
-half.width <- qt(1-alpha_level/2, n-1)*s/sqrt(n)
+mean_lamina <- mean(traits$lamina_area_cm2) #sample mean
+sd_lamina <- sd(traits$lamina_area_cm2) #sample standard deviation
+n <- length(traits$lamina_area_cm2) #sample size
+half.width <- qt(1-alpha_level/2, n-1)*sd_lamina/sqrt(n) #tail points of t distrib.
+
 # Confidence Interval
-c(xbar - half.width, xbar + half.width)
-
-qqnorm(traits$lamina_area_cm2)
+ci_lamina <- c(xbar - half.width, xbar + half.width)
 
 
-# simple linear regression ------------------------------------------------
 
-##lets assume that our previous plot looked remotely linear
-model <- lm(frond_length_cm~ lamina_area_cm2, data=traits)
-summary(model)
 
+# Hypothesis testing -----------
+
+#we may ask if fern leaf size is consistent with global leaf size
+t.test(, mu=315, conf.level = 0.90) #i made this number up
+
+# depending on our data type, we usually check for a normal distribution
+hist()
+
+#we can run non-parametric tests if we determine a ttest is not appropriate
+wilcox.test(, mu=315)
+
+# compare 2 populations of frond length
+hist(traits$frond_length_cm) #data looks pretty good
+levels(traits$niche)
+
+#practive making 2 objects of frond length for epiphytes and terrestrial species 
+frond_epi <- 
+frond_terr <- 
+
+#compare frond length between the two niches
+t.test(, var.equal = TRUE)
+
+
+
+
+# simple linear regression -----------
+
+#we visualized this relationship earlier...
 plot(frond_length_cm ~ lamina_area_cm2, data=traits)
-abline()
 
-##we can plot the model object to get some diagnostics of the fit
-plot(model)
 
-library(car)
-qqPlot(model) #with confidence intervals to better understand departures from normality
-norm <- residuals(model)
+# lets assume that our previous plot looked remotely linear
+# lets setup a linear model investigated  the linear relationship between them
+
+leaf_model <- lm()
+summary(leaf_model)
+
+# It is necessary to examine how well our model fits the data
+# R provides many ways to do this, man of them based on the residuals
+
+#diagnoistics plots 
+
+plot(leaf_model) #there should be not structure on the scale-location plot
+norm <- residuals(leaf_model)
 hist(norm)
-#R points out the row numbers of the outliers on diagnostic plots
 
-###we could perform a data transformation
-logmodel <- lm(log10(frond_length_cm) ~ log10(lamina_area_cm2), data=traits)
-summary(logmodel)
-plot(logmodel)
+# the car package has some of the best ones
+library(car)
+qqPlot() #uses confidence intervals to see departures from normality
+residualPlot()
 
+#often you may need a data transformation, lets visualize with a log transformation
+
+windows()
 plot(frond_length_cm ~ lamina_area_cm2, data=traits, log='xy')
-abline(logmodel)
 
-###Lets remake our figure with regression line
-###regression line CAN NOT exceed data points, so we will use another package function
+#lets refit the model with the log transformation
+logmodel <- lm()
 
-LAlabel <- expression(Lamina~area~~(cm^2))
-FLlabel <- "Frond Length (cm)"
+qqPlot(logmodel) 
+residualPlot(logmodel)
+#R points out the row numbers of possible outliers on diagnostic plots
+
+#it is easy to extract bits from the model summary
+summary()
+coef()
+
+#lots of things you can extract
+str(summary())
+
+#lets keep the R2 value
+myr2 <- summary()$r.squared
+
+
+# lets remake our figure with a regression line
+plot(log10(frond_length_cm) ~ log10(lamina_area_cm2), data=traits)
+abline() #draws a line from point a - b (look at help file)
+
+
+# regression line CAN NOT exceed data points, 
+# so we will use another package function from plotrix
 library(scales)
 library(plotrix)
+
+#make some nice colors and labels
+LAlabel <- expression(Lamina~area~~(cm^2))
+FLlabel <- "Frond Length (cm)"
+
+mycols <- c("firebrick", "forestgreen", "dodgerblue")
 mycols2 <- alpha(mycols, .75)
 
-#extract the R2 value
-r2mod <- round(summary(model)$r.squared, 2)
-r2lab <- paste(expression(R^2), r2mod, sep = " = ")
+# make a nice r2 object to place on the graph
+r2lab <- paste(expression(R^2), round(myr2,2), sep = " = ")
 
-#R prints things on plots in the order you code them, so for practice I want you to put 
-## regression line underneath points
-## We will need to make an empty plot first
+# R prints things on plots in the order you code them, 
+# so for practice I want to put regression line underneath points
+# I will need to make an empty plot first = type='n'
 
 windows(7,7)
 par(mar=c(4,4,1,1), cex.axis=.8, cex.lab=1.1, mgp=c(2.5, 1, 0))
 
-plot(frond_length_cm ~ lamina_area_cm2, data=traits,ylim=c(0, 180), xlim=c(0, 1800),
-      ylab=FLlabel, xlab=LAlabel, type = 'n') #type ='n' sets empty plots
+plot(log10(frond_length_cm) ~ log10(lamina_area_cm2), data=traits,
+     ylab=FLlabel, xlab=LAlabel, type = 'n') 
 
-ablineclip(model2, x1=min(traits$lamina_area_cm2), x2=max(traits$lamina_area_cm2),
-           lty=2, lwd=2)
+points()
 
-points(frond_length_cm ~ lamina_area_cm2, data=traits, bg=mycols2[niche], pch=21, cex=1.5)
-#points has the same formular as plotting, but just prints the points
+ablineclip()
+
 legend("bottomright", levels(traits$niche), pch=21,inset=0.01, 
        pt.bg=mycols2, bty='n')
 
-#adding text to the figure
-text(1600, 170, r2lab)
+#adding r2 object to the figure
+text(1.5, 2.1, r2lab )
